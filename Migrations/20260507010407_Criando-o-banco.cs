53ee34edafaf9b31f9c107cb05ab4af6b4696f46
coimbrajoao <coimbrajoao235@gmail.com>
2026-05-06 22:16:18 -0300

    Feat: Impementando os dados de orçamento e repositorio.

---
+++ Migrations/20260507010407_Criando-o-banco.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace OficinaMecanica.Migrations
+{
+    /// <inheritdoc />
+    public partial class Criandoobanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Budgets",
+                columns: table => new
+                {
+                    IdBudget = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ClintId = table.Column<int>(type: "int", nullable: false),
+                    vehicle = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budgets", x => x.IdBudget);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BudGetItens",
+                columns: table => new
+                {
+                    idBudGetItens = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    ValorUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    BudGetId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudGetItens", x => x.idBudGetItens);
+                    table.ForeignKey(
+                        name: "FK_BudGetItens_Budgets_BudGetId",
+                        column: x => x.BudGetId,
+                        principalTable: "Budgets",
+                        principalColumn: "IdBudget",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudGetItens_BudGetId",
+                table: "BudGetItens",
+                column: "BudGetId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BudGetItens");
+
+            migrationBuilder.DropTable(
+                name: "Budgets");
+        }
+    }
+}
